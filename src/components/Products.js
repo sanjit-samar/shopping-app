9537414c147fa6e7e31a4df039c4ab1a51224659
sanjit samar <sanjit1525@gmail.com>
2024-03-17 18:57:37 +0530

    disabled btn bug fix

--- src/components/Products.js
+++ src/components/Products.js
@@ -5,7 +5,7 @@ const DUMMY_PRODUCTS = [
     id: 1,
     name: "MacBook",
     imgURL:
-      "https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bGFwdG9wfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    "https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bGFwdG9wfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
     price: 25,
   },
   {
